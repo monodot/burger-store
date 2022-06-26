7eeaae64bbf2de0c5456d41de276a38483b40141
Tom Donohue <monodot@gmail.com>
2022-06-26 18:53:37 +0100

    Add some logz

--- src/main/java/com/tutorialworks/burgerstore/ApiController.java
+++ src/main/java/com/tutorialworks/burgerstore/ApiController.java
@@ -1,5 +1,7 @@
 package com.tutorialworks.burgerstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +11,18 @@ import java.util.List;
 @RestController
 public class ApiController {
 
+    private Logger LOGGER = LoggerFactory.getLogger(ApiController.class);
+
     @GetMapping("/menu")
     List<MenuItem> getMenu() {
+        LOGGER.trace("getMenu called");
+
         List<MenuItem> menu = new ArrayList<MenuItem>();
         menu.add(new MenuItem("Vegan Shape", 350));
         menu.add(new MenuItem("Deluxe Double Cheese Burger", 850));
         menu.add(new MenuItem("Basic Burger", 450));
 
+        LOGGER.debug("Returning menu");
         return menu;
     }
 
