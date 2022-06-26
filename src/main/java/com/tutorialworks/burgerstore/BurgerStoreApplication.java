4db0900a6d04f1fa3c1c64a36ad926d70347c65e
Tom Donohue <monodot@gmail.com>
2022-06-26 13:52:14 +0100

    Initial commit 🍔

---
+++ src/main/java/com/tutorialworks/burgerstore/BurgerStoreApplication.java
@@ -0,0 +1,13 @@
+package com.tutorialworks.burgerstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorialworks/burgerstore/BurgerStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorialworks.burgerstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
